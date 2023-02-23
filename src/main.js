ff9c84227a4681be07577379ea0b771f7532f1d2
kachan <naer0107@163.com>
2023-02-23 11:41:14 +0800

    解决头像图片异常

--- src/main.js
+++ src/main.js
@@ -12,23 +12,17 @@ import App from './App'
 import store from './store'
 import router from './router'
 
+import * as directives from '@/directives' // 自定义指令
 import '@/icons' // icon
 import '@/permission' // permission control
 
-/**
- * If you don't want to use mock-server
- * you want to use MockJs for mock api
- * you can execute: mockXHR()
- *
- * Currently MockJs will be used in the production environment,
- * please remove it before going online ! ! !
- */
-
-// set ElementUI lang to EN
 Vue.use(ElementUI, { locale })
 // 如果想要中文版 element-ui，按如下方式声明
 // Vue.use(ElementUI)
-
+// 注册自定义指令
+Object.keys(directives).forEach(key => {
+  Vue.directive(key, directives[key])
+})
 Vue.config.productionTip = false
 
 new Vue({
